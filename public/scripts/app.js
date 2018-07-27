e9b0286f09af0a672b27bdb39e01b347d09d4a2f
mplouffe <matheuplouffe@gmail.com>
2018-07-27 16:05:07 -0500

    Initial Commit.

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+console.log('App.js is running!');
+
+// JSX - Javascript XML
+//var template = <p>This is JSX from app.js!</p>;
+
+var template = React.createElement(
+    "p",
+    null,
+    "This is JSX from app.js!"
+  );
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
