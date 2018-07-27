7f5e3c6677088a61c3c70eada30f3609ce5b23ea
mplouffe <matheuplouffe@gmail.com>
2018-07-27 16:27:30 -0500

    Added Babel to the project.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,14 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX - Javascript XML
-//var template = <p>This is JSX from app.js!</p>;
-
 var template = React.createElement(
-    "p",
-    null,
-    "This is JSX from app.js!"
-  );
+  'p',
+  null,
+  'That is super cool.'
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log('App.js is running!');
+
+// JSX - Javascript XML
+var template = <p>That is super cool.
+</p>;
+
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
