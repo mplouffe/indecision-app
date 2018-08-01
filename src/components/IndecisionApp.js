150046b0c3bc05d95b3509e8ee034e78ebc5f381
mplouffe <matheuplouffe@gmail.com>
2018-08-01 15:13:20 -0500

    Added class properties to Babel and converted class components to take advantage of them.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,16 +5,34 @@ import Header from './Header';
 import Action from './Action';
 
 class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
-            options: []
-        }
+    state = {
+        options: []
     }
+    // EVENT HANDLERS
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: [] }));
+    };
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => {
+                return option !== optionToRemove;
+            })
+        }));
+    };
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        alert(this.state.options[randomNum]);
+    };
+    handleAddOption = (option) => {
+        if(!option) {
+            return 'Enter valid value to add item';
+        } else if(this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
+        } 
+
+        this.setState((prevState) => ({ options: prevState.options.concat([option]) }));
+    };
+
     // LIFECYCLE METHODS
     componentDidMount() {
         try{
@@ -32,33 +50,6 @@ class IndecisionApp extends React.Component {
             const json = JSON.stringify(this.state.options);
             localStorage.setItem('options', json);
         }
-    }
-    compoenentWillUnmount() {
-        
-    }
-    // EVENT HANDLERS
-    handleDeleteOptions() {
-        this.setState(() => ({ options: [] }));
-    }
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => {
-                return option !== optionToRemove;
-            })
-        }));
-    }
-    handlePick() {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        alert(this.state.options[randomNum]);
-    }
-    handleAddOption(option){
-        if(!option) {
-            return 'Enter valid value to add item';
-        } else if(this.state.options.indexOf(option) > -1) {
-            return 'This option already exists';
-        } 
-
-        this.setState((prevState) => ({ options: prevState.options.concat([option]) }));
     }
     render() {
         const subtitle = 'Decisiveness for the Indecisive';
