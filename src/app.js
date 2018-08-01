e550f9ab04b91cbc3771e2f3ef8a7327a9d3781d
mplouffe <matheuplouffe@gmail.com>
2018-08-01 16:04:36 -0500

    Starting to implement SCSS to style the project.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
+import './styles/styles.scss';
 
-const Layout = () => {
-    return (
-        <div>
-            <p>header</p>
-            <p>footer</p>
-        </div>
-    );
-}
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
